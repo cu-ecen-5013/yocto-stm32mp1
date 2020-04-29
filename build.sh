18899f5a7b68713ee9c3af9105f7430088f4562b
bcis93 <bryancisneros93@gmail.com>
2020-04-29 16:29:15 -0600

    Updating build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,18 @@ git submodule update --init --recursive
 # TODO: Change the machine name and distribution if you are making changes to that.
 DISTRO=openstlinux-weston MACHINE=stm32mp1 source ./layers/meta-st/scripts/envsetup.sh
 
+if grep -q "virtualization" ./conf/local.conf; then
+    echo -n ""
+else
+    echo "DISTRO_FEATURES_append=\" virtualization\"" >> ./conf/local.conf
+fi
+
+if grep -q "PACKAGECONFIG_pn-azure-iot-sdk-c_append" ./conf/local.conf; then
+    echo -n ""
+else
+    echo "PACKAGECONFIG_pn-azure-iot-sdk-c_append = \"amqp mqtt uhttp edge\"" >> ./conf/local.conf
+fi
+
 bitbake-layers show-layers | grep "meta-oe" > /dev/null
 layer_info=$?
 if [ $layer_info -ne 0 ];then
@@ -28,6 +40,43 @@ if [ $layer_info -ne 0 ];then
 	bitbake-layers add-layer ../layers/meta-openembedded/meta-python
 else
 	echo "meta-python layer already exists"
+fi
+
+
+bitbake-layers show-layers | grep "meta-filesystems" > /dev/null
+layer_info=$?
+if [ $layer_info -ne 0 ];then
+	echo "Adding meta-filesystems layer"
+	bitbake-layers add-layer ../layers/meta-openembedded/meta-filesystems
+else
+	echo "meta-filesystems layer already exists"
+fi
+
+bitbake-layers show-layers | grep "meta-virtualization" > /dev/null
+layer_info=$?
+if [ $layer_info -ne 0 ];then
+	echo "Adding meta-virtualization layer"
+	bitbake-layers add-layer ../layers/meta-virtualization
+else
+	echo "meta-virtualization layer already exists"
+fi
+
+bitbake-layers show-layers | grep "meta-rust" > /dev/null
+layer_info=$?
+if [ $layer_info -ne 0 ];then
+	echo "Adding meta-rust layer"
+	bitbake-layers add-layer ../layers/meta-rust
+else
+	echo "meta-rust layer already exists"
+fi
+
+bitbake-layers show-layers | grep "meta-iotedge" > /dev/null
+layer_info=$?
+if [ $layer_info -ne 0 ];then
+	echo "Adding meta-iotedge layer"
+	bitbake-layers add-layer ../layers/meta-iotedge
+else
+	echo "meta-iotedge layer already exists"
 fi
 
 # TODO: Add more layers in the similar format
