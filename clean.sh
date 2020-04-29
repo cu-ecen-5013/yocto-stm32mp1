80537d33231fa6f18d25dd95a72b63a33129f3b7
Atharva Nandanwar <atharva.nandanwar@colorado.edu>
2020-04-28 23:41:23 -0600

    Updated flash script with comments; added clean.sh

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Name - Atharva Nandanwar
+# File - clean.sh
+# Purpose - cleans all the build
+
+cd `dirname $0`
+
+rm -rf build-*
+
--- flash.sh
+++ flash.sh
@@ -4,8 +4,6 @@
 # File - flash.sh
 # Purpose - Used to flash code into STM32MP1 connected through usb
 
-# Check if the driver and rules exist
-
 curr_dir=`dirname $0`
 build_dir="build-openstlinuxweston-stm32mp1-aesd-project"
 image_dir="tmp-glibc/deploy/images/stm32mp1-aesd-project"
@@ -13,6 +11,7 @@ flash_parent_dir="flashlayout_st-image-aesd"
 flash_layout_file="FlashLayout_sdcard_stm32mp157a-aesd-project-mx-trusted.tsv"
 stm_flasher="${curr_dir}/STM32CubeProgrammer-2.4.0/bin/STM32_Programmer_CLI"
 
+# Check if the driver and rules exist
 # Check if necessary drivers are installed
 dpkg -S libusb-1.0-0 > /dev/null
 if [[ $? -ne 0 ]]; then
